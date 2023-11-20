8205a9e593cdd952de620dffb82c99081ab8f14e
Erkin Ata Yazıcı <erkinatayazici@gmail.com>
2023-11-20 15:52:59 +0300

    fourth version

--- ceng105.c
+++ ceng105.c
@@ -7,8 +7,8 @@ float num1, Celsius;
 
 
 
- 
- printf("enter the temperature in fahrenheit=");
+ //
+ printf("Enter the Temperature in Fahrenheit=");
  scanf("%f", &num1);
     Celsius= (num1-32)*5/9;
    printf("%.2f degrees fahrenheit is equal to: %.2f in celsius" ,num1 ,Celsius);
